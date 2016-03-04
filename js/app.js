aa19b3f0fcf6a69c0514b29b3476566b49d625b1
Karl Diab <A00805534@ad.bcit.ca>
2016-03-04 10:25:54 -0800

    broke the hell out of angularjs app... to be continued

--- js/app.js
+++ js/app.js
@@ -175,3 +175,37 @@ function data($scope, $http) {
         $scope.drawChart(true);
     }
 }
+var app = angular.module('store', []);
+app.controller("ReviewController", function(){
+	this.review = {};
+	this.addReview = function(product) {
+		this.review.createdOn = Date.now();
+		product.reviews.push(this.review);
+		this.review = {};
+	}
+});
+
+app.directive("productTitle", function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'product-title.html'
+	};
+	
+});
+app.directive("productPanels", function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'product-panels.html',
+		controller: function(){
+			this.tab = 1;
+			
+			this.selectTab = function(setTab) {
+				this.tab = setTab;
+			}
+			this.isSelected = function(checkTab) {
+				return this.tab == checkTab;
+			}
+		},
+		controllerAs: 'panel'	
+	}
+})
